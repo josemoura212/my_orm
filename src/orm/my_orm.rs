8b5a5d0b0e892277087c18d427fe889fa6026f09
José Augusto <josemoura212@gmail.com>
2024-05-13 13:31:03 -0300

    chore: Remove commented out code and unused import in my_orm.rs

--- src/orm/my_orm.rs
+++ src/orm/my_orm.rs
@@ -1,3 +1,5 @@
+use std::process::id;
+
 use inflector::Inflector;
 use mysql::{prelude::Queryable, Pool, PooledConn};
 
@@ -58,149 +60,17 @@ impl MyORM {
         println!("===============================");
         conn.query_drop(sql).unwrap();
     }
+
+    pub fn incluir<T: HasId>(&self, model: &T) -> i32 {
+        let mut conn = self.get_conn();
+
+        let table_name = self.table_name(model);
+        let campos = model.campos_model();
+
+        let sql = format!("INSERT INTO {}s ", table_name,);
+
+        let result = conn.query_first(sql).unwrap();
+
+        result.unwrap()
+    }
 }
-
-//     pub fn get_all(&self) -> Option<Vec<Aluno>> {
-//         let mut conn = self.get_conn();
-
-//         let query = "SELECT
-//                                 alu.matricula,
-//                                 alu.nome,
-//                             group_concat(nota.materia,':',nota.nota separator ';') as notas
-//                             FROM aluno alu inner join nota on alu.matricula = nota.matricula
-//                             GROUP BY alu.matricula;";
-//         let alunos: Vec<Aluno> = conn
-//             .query_map(
-//                 query,
-//                 |(matricula, nome, notas): (String, String, String)| Aluno {
-//                     matricula,
-//                     nome,
-//                     notas: notas
-//                         .split(';')
-//                         .map(|nota| {
-//                             let mut nota = nota.split(':');
-//                             AlunoNota {
-//                                 disciplina: nota.next().unwrap().to_string(),
-//                                 nota: nota.next().unwrap().parse().unwrap(),
-//                             }
-//                         })
-//                         .collect(),
-//                 },
-//             )
-//             .unwrap();
-
-//         Some(alunos)
-//     }
-
-//     pub fn get_by_matricula(&self, matricula: String) -> Option<Aluno> {
-//         let mut conn = self.get_conn();
-
-//         let query = "SELECT
-//                                 alu.matricula,
-//                                 alu.nome,
-//                             group_concat(nota.materia,':',nota.nota separator ';') as notas
-//                             FROM aluno alu inner join nota on alu.matricula = nota.matricula
-//                             WHERE alu.matricula = :matricula
-//                             GROUP BY alu.matricula;";
-//         let aluno: Option<Aluno> = conn
-//             .exec_first(
-//                 query,
-//                 params! {
-//                     "matricula" => &matricula
-//                 },
-//             )
-//             .unwrap()
-//             .map(|(matricula, nome, notas): (String, String, String)| Aluno {
-//                 matricula,
-//                 nome,
-//                 notas: notas
-//                     .split(';')
-//                     .map(|nota| {
-//                         let mut nota = nota.split(':');
-//                         AlunoNota {
-//                             disciplina: nota.next().unwrap().to_string(),
-//                             nota: nota.next().unwrap().parse().unwrap(),
-//                         }
-//                     })
-//                     .collect(),
-//             });
-
-//         aluno
-//     }
-
-//     pub fn save(&self, aluno: Aluno) {
-//         let mut conn = self.get_conn();
-//         let mut tx = conn.start_transaction(TxOpts::default()).unwrap();
-
-//         tx.exec_drop(
-//             "INSERT INTO aluno (matricula, nome) VALUES (:matricula, :nome)",
-//             params! {
-//                 "matricula" => &aluno.matricula,
-//                 "nome" => aluno.nome,
-//             },
-//         )
-//         .unwrap();
-
-//         for nota in &aluno.notas {
-//             tx.exec_drop(
-//                 "INSERT INTO nota (matricula, materia, nota) VALUES (:matricula, :materia, :nota)",
-//                 params! {
-//                     "matricula" => &aluno.matricula,
-//                     "materia" => &nota.disciplina,
-//                     "nota" => &nota.nota,
-//                 },
-//             )
-//             .unwrap();
-//         }
-
-//         tx.commit().unwrap();
-//     }
-
-//     pub fn change(&self, aluno: Aluno) {
-//         let mut conn = self.get_conn();
-
-//         conn.exec_drop(
-//             "UPDATE aluno SET nome = :nome WHERE matricula = :matricula",
-//             params!(
-//                 "nome" => aluno.nome,
-//                 "matricula" => &aluno.matricula
-//             ),
-//         )
-//         .unwrap();
-
-//         for nota in &aluno.notas {
-//             conn.exec_drop(
-//                 "UPDATE nota SET nota = :nota WHERE matricula = :matricula AND materia = :materia",
-//                 params!(
-//                     "nota" => &nota.nota,
-//                     "matricula" => &aluno.matricula,
-//                     "materia" => &nota.disciplina
-//                 ),
-//             )
-//             .unwrap();
-//         }
-//     }
-
-//     pub fn delete(&self, matricula: String) {
-//         let mut conn = self.get_conn();
-//         let mut tx = conn.start_transaction(TxOpts::default()).unwrap();
-
-//         tx.exec_drop(
-//             "DELETE FROM nota WHERE matricula = :matricula",
-//             params!(
-//                 "matricula" => &matricula
-//             ),
-//         )
-//         .unwrap();
-
-//         tx.exec_drop(
-//             "DELETE FROM aluno WHERE matricula = :matricula",
-//             params!(
-//                 "matricula" => &matricula
-//             ),
-//         )
-//         .unwrap();
-
-//         tx.commit().unwrap();
-//     }
-// }
